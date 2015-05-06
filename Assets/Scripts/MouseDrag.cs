5ca584b6fb8b0b8b80e364600b04187c4ebcd55f
bitinvert <toxitygamer@gmail.com>
2015-05-06 20:22:46 +0200

    Adding scripts and oculus support

---
+++ Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseDrag : MonoBehaviour {
+
+	void OnMouseDrag()
+	{
+		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+		Vector3 pos_move = Camera.main.ScreenToWorldPoint(
+			new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen ));
+
+		transform.position = new Vector3( pos_move.x, transform.position.y, pos_move.z );
+	}
+}
