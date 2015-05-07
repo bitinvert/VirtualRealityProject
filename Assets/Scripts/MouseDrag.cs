f696b8e97c2cbe1a7e05dba73869a272c9aabc1f
bitinvert <toxitygamer@gmail.com>
2015-05-07 12:27:03 +0200

    added Leap support

--- Assets/Scripts/MouseDrag.cs
+++ Assets/Scripts/MouseDrag.cs
@@ -2,13 +2,15 @@
 using System.Collections;
 
 public class MouseDrag : MonoBehaviour {
-
+	
 	void OnMouseDrag()
 	{
-		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-		Vector3 pos_move = Camera.main.ScreenToWorldPoint(
-			new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen ));
 
-		transform.position = new Vector3( pos_move.x, transform.position.y, pos_move.z );
+			float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+			Vector3 pos_move = Camera.main.ScreenToWorldPoint(
+				new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen ));
+
+			transform.position = new Vector3( pos_move.x, transform.position.y, pos_move.z );
+
 	}
 }
