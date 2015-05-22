a0b2c00a9bc860a886aa9033a1e2bd8a2a8d959b
bitinvert <toxitygamer@gmail.com>
2015-05-22 21:37:47 +0200

    Modified Grabbable object
    made Testcube into a prefab

--- Assets/ExternalAssets/LeapMotion/Scripts/Utils/GrabbableObject.cs
+++ Assets/ExternalAssets/LeapMotion/Scripts/Utils/GrabbableObject.cs
@@ -3,64 +3,76 @@
 * Leap Motion proprietary. Licensed under Apache 2.0                           *
 * Available at http://www.apache.org/licenses/LICENSE-2.0.html                 *
 \******************************************************************************/
-
+/*Modified*/
 using UnityEngine;
 using System.Collections;
 
 public class GrabbableObject : MonoBehaviour {
-
-  public bool useAxisAlignment = false;
-  public Vector3 rightHandAxis;
-  public Vector3 objectAxis;
-
-  public bool rotateQuickly = true;
-  public bool centerGrabbedObject = false;
-
-  public Rigidbody breakableJoint;
-  public float breakForce;
-  public float breakTorque;
-
-  protected bool grabbed_ = false;
-  protected bool hovered_ = false;
-
-  public bool IsHovered() {
-    return hovered_;
-  }
-
-  public bool IsGrabbed() {
-    return grabbed_;
-  }
-
-  public virtual void OnStartHover() {
-    hovered_ = true;
-  }
-
-  public virtual void OnStopHover() {
-    hovered_ = false;
-  }
-
-  public virtual void OnGrab() {
-    grabbed_ = true;
-    hovered_ = false;
-
-    if (breakableJoint != null) {
-      Joint breakJoint = breakableJoint.GetComponent<Joint>();
-      if (breakJoint != null) {
-        breakJoint.breakForce = breakForce;
-        breakJoint.breakTorque = breakTorque;
-      }
-    }
-  }
-
-  public virtual void OnRelease() {
-    grabbed_ = false;
-
-    if (breakableJoint != null) {
-      Joint breakJoint = breakableJoint.GetComponent<Joint>();
-      if (breakJoint != null) {
-        breakJoint.breakForce = Mathf.Infinity;
-        breakJoint.breakTorque = Mathf.Infinity;
-      }
-    }
-  }
+	
+	public GridSnap gs;
+	
+	
+	public bool useAxisAlignment = false;
+	public Vector3 rightHandAxis;
+	public Vector3 objectAxis;
+	
+	public bool rotateQuickly = true;
+	public bool centerGrabbedObject = false;
+	
+	public Rigidbody breakableJoint;
+	public float breakForce;
+	public float breakTorque;
+	
+	protected bool grabbed_ = false;
+	protected bool hovered_ = false;
+	
+	public bool IsHovered() {
+		return hovered_;
+	}
+	
+	public bool IsGrabbed() {
+		return grabbed_;
+	}
+	
+	public virtual void OnStartHover() {
+		hovered_ = true;
+	}
+	
+	public virtual void OnStopHover() {
+		hovered_ = false;
+	}
+	
+	public virtual void OnGrab() {
+		grabbed_ = true;
+		hovered_ = false;
+		
+		this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+		
+		if (breakableJoint != null) {
+			Joint breakJoint = breakableJoint.GetComponent<Joint>();
+			if (breakJoint != null) {
+				breakJoint.breakForce = breakForce;
+				breakJoint.breakTorque = breakTorque;
+			}
+		}
+		
+	}
+	
+	public virtual void OnRelease() {
+		grabbed_ = false;
+		
+		if (breakableJoint != null) {
+			Joint breakJoint = breakableJoint.GetComponent<Joint>();
+			if (breakJoint != null) {
+				breakJoint.breakForce = Mathf.Infinity;
+				breakJoint.breakTorque = Mathf.Infinity;
+				
+			}
+		}
+		//Anm.: Hier WinkelSnap noch machen!!! naechste zeile nur ersatz
+		this.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+		this.transform.position = gs.SnapToGrid(this.transform.position);
+		this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+	}
+	
 }
