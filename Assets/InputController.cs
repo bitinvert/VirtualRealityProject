9fb5d4022a84152eb761b2d1c6ce497c284e866d
bitinvert <toxitygamer@gmail.com>
2015-06-05 17:29:33 +0200

    Yolo

---
+++ Assets/InputController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+
+	LevelController lc;
+
+	void Start() {
+		lc = this.GetComponent<LevelController>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Escape))
+		{
+			Debug.Log("Quit");
+			Application.Quit();
+		}
+		if(Input.GetKey(KeyCode.P)) {
+			lc.Check();
+		}
+	}
+}
---
+++ Assets/LevelController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelController : MonoBehaviour {
+
+	GameObject triCorner;
+	GameObject quadCorner;
+	GameObject quadTee;
+
+	void Start() {
+		triCorner = GameObject.FindGameObjectWithTag("TriCorner");
+		quadCorner = GameObject.FindGameObjectWithTag("QuadCorner");
+		quadTee = GameObject.FindGameObjectWithTag("QuadTee");
+	}
+
+	public void Check() {
+		//First Correct Option
+		Vector3 triRot = triCorner.transform.eulerAngles;
+		if(Mathf.RoundToInt(triRot.x) == 270 && Mathf.RoundToInt(triRot.y) == 180 && Mathf.RoundToInt(triRot.z) == 0 
+		   || Mathf.RoundToInt(triRot.x) == 90 && Mathf.RoundToInt(triRot.y) == 90 && Mathf.RoundToInt(triRot.z) == 0) {
+			Debug.Log ("yay :3");
+		} else {
+			Debug.Log ("meh");
+		}
+
+	}
+}
