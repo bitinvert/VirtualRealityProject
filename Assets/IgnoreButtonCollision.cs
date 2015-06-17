ec6fbd732045baed83701e4d7a8710b885340a7b
Chelinka <kasumi5@gmx.de>
2015-06-17 15:57:26 +0200

    Some Bugfixes, better Performance
    - Added Script to prevent GamePieces to collide with menuButtons
    - cleaned up hirachy
    - changed lightning
    - changed backWall's material to Wall Material
    - disabled some lights to improve performance

---
+++ Assets/IgnoreButtonCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnoreButtonCollision : MonoBehaviour {
+
+	/**
+	 * Disable Collision between the MenuButtons (Layer 8)
+	 * and the GamingPieces (Layer 9) in order to prevent
+	 * unintentional "Button-Pushing".
+	 * Also: Disable Collision between MenuButtons (Layer 8)
+	 * and the ColliderWall in Front of the Camera (Layer 10)
+	 * in order to prevent the Menu Button being stuck behind
+	 * the ColliderWall if the player is successful in
+	 * pushing it through (which would be a bug).
+	 */
+	void Start () {
+		Physics.IgnoreLayerCollision (8, 9);
+		Physics.IgnoreLayerCollision (8, 10);
+	}
+	
+
+}
--- Assets/Scripts/MouseDrag.cs
+++ Assets/Scripts/MouseDrag.cs
@@ -5,7 +5,6 @@ public class MouseDrag : MonoBehaviour {
 	
 	void OnMouseDrag()
 	{
-
 			float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
 			Vector3 pos_move = Camera.main.ScreenToWorldPoint(
 				new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen ));
