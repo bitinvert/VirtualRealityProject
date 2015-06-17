53d48c85079d047386f415883819b446560566bc
Chelinka <kasumi5@gmx.de>
2015-06-17 21:02:59 +0200

    Added Adjustable Camera Height Script
    OVR Camera's height (y pos) is now adjustable through pageUp and
    pageDown KeyButtons

---
+++ Assets/AdjustableCameraHeight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustableCameraHeight : MonoBehaviour {
+	
+	/**
+	 * Makes the Camera's height adjustable per PageUp
+	 * or PageDown.
+	 */
+	void Update () {
+		Vector3 pos = this.gameObject.transform.position;
+
+		if (Input.GetKey (KeyCode.PageUp) && (pos.y < 8f)) 
+		{
+			pos.y += 0.1f;
+
+		} else if (Input.GetKey (KeyCode.PageDown) && (pos.y > 5f)) 
+		{
+			pos.y -= 0.1f;
+		}
+
+		this.gameObject.transform.position = pos;
+	}
+}
